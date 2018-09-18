e630ad36a919eee73c0d6518caa2a4fc961e3b7f
Jung Hur <junghur@college.harvard.edu>
2018-09-17 20:56:21 -0400

    back button working

--- index.js
+++ index.js
@@ -1,30 +1,147 @@
 const API_KEY = "8d33e1b0-b77e-11e8-bf0e-e9322ccde4db";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
-  showGalleries(url);
+    const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
+    showGalleries(url);
 });
 
-function showGalleries(url) {
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    data.records.forEach(gallery => {
-      document.querySelector("#galleries").innerHTML += `
-        <li>
-          <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
-            Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
-          </a>
-        </li>
-      `;
-    });
-    if (data.info.next) {
-      showGalleries(data.info.next);
-    }
-  })
+// // Every time the URL changss
+// window.onhashchange = () => {
+//     window.addEventListener( () => {
+//
+//         if (window.location.hash) {
+//             let hash = window.location.hash.slice(1);
+//             urlChange(hash);
+//         }
+//     })
+// }
+//
+// function urlChange(hash)
+// {
+//     new_url = `href=`
+//         window.location.hash = hash;
+// }
+
+function goBackToGallery() {
+    document.querySelector("#all-objects").style.display = "none";
+    document.querySelector("#all-galleries").style.display = "block";
+    document.querySelector("#single-object").style.display = "none";
 }
 
-function showObjectsTable(id) {
-  document.querySelector("#all-objects").style.display = "block";
-  document.querySelector("#all-galleries").style.display = "none";
+function goBackToObject() {
+    document.querySelector("#all-objects").style.display = "block";
+    document.querySelector("#all-galleries").style.display = "none";
+    document.querySelector("#single-object").style.display = "none";
 }
+
+function showGalleries(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.records.forEach(gallery => {
+
+                // x += x.assign(location.hash);
+
+                document.querySelector("#galleries").innerHTML += `
+            <li>
+              <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
+                Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+              </a>
+            </li>
+          `;
+            });
+
+            if (data.info.next) {
+                showGalleries(data.info.next);
+            }
+
+        });
+}
+
+
+function showObjectsTable(id) {
+    document.querySelector("#all-objects").style.display = "block";
+    document.querySelector("#all-galleries").style.display = "none";
+    document.querySelector("#single-object").style.displayer = "none";
+    document.querySelector("#objects").innerHTML = "";
+
+    const obj_url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${id}`
+    moreObjects(obj_url)
+}
+
+function moreObjects(obj_url) {
+    fetch(obj_url)
+        .then(objResponse => objResponse.json())
+        .then(objData => {
+            objData.records.forEach(object => {
+                var allpeople = "";
+
+                if (object.people !== "null") {
+                    object.people.forEach(person => {
+                        allpeople += person.name + ", ";
+                    });
+                } else {
+                    allpeople = "None found";
+                }
+
+
+                document.querySelector("#objects").innerHTML += `
+                                    <li>
+                                    <a href="#${object.objectnumber}" onclick="showSingleTable('${object.objectnumber}')">
+                                    Title: ${object.title}.
+                                    <br> 
+                                    People involved: ${allpeople}
+                                    
+                                    <br>
+                                    URL: ${object.url}
+                                    <br>
+                                    Image: <img src="${object.primaryimageurl}" class="picture">
+                                    </a>
+                                    <br>
+                                    </li>
+                                    `;
+            });
+            if (objData.info.next) {
+                moreObjects(objData.info.next);
+            }
+        });
+}
+
+
+function showSingleTable(id) {
+
+    document.querySelector("#single-object").style.display = "block";
+    document.querySelector("#all-objects").style.display = "none";
+    document.querySelector("#all-galleries").style.display = "none";
+
+
+    const obj_url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&objectnumber=${id}`;
+
+    fetch(obj_url)
+        .then(objResponse => objResponse.json())
+        .then(objData => {
+            objData.records.forEach(object => {
+                document.querySelector("#single").innerHTML = `
+                                    <strong> Title: </strong> ${object.title}
+                                    <br>
+                                    <img src="${object.primaryimageurl}" class="picture">
+                                    <br>
+                                    <strong> Description: </strong> ${object.desription}
+                                    <br> 
+                                    <strong> Label:</strong> ${object.labeltext}
+                                    <br>
+                                    <strong> People involved: </strong> ${object.people[0].name}
+                                    <br>
+                                    <strong> Provenance: </strong> ${object.provenance}
+                                    <br>
+                                    <strong> Accession year: </strong> ${object.accessionyear}
+                                    <br>
+                                    <a href="${object.url}"> ${object.url} </a>
+                                    <br>`;
+            });
+        });
+}
+
+
+
+
